9d1e0b4b5cf69137fe2b2918c9147d1b7bd787fb
TailSpin <37714042+Serunya@users.noreply.github.com>
2022-03-19 15:07:17 +0300

    Rest Api from Center Invest Bank

--- t-invest-api.py
+++ t-invest-api.py
@@ -3,12 +3,7 @@ from datetime import datetime
 
 # https://github.com/Fatal1ty/tinkoff-api
 
-from tinkoff.investments import (
-    CandleResolution,
-    Environment,
-    TinkoffInvestmentsRESTClient,
-)
-from tinkoff.investments.utils.historical_data import HistoricalData
+
 
 
 async def get_minute_candles():
@@ -17,7 +12,6 @@ async def get_minute_candles():
         token="t.0d0gyqxYtnN4fZSlh7cdORCpIJ6TNxwk5Ksehg-FOxfRoZGdSeW0TNaLMIeMiZNVQTwUpcWjNGa4AEe7bDzcBQ", environment=Environment.SANDBOX
     ) as client:
         bonds = client.market.instruments.get_bonds()
-        ):
             print(bonds)
 
 
