0d89d42f1732ac307dbc1abffbc78673a25d704e
Malinka <101825681+OnlyMalinka@users.noreply.github.com>
2022-03-19 17:29:05 +0300

    импут через апи

--- obligacii.py
+++ obligacii.py
@@ -2,35 +2,44 @@ import InvestApi
 
 a = InvestApi.getAllBonds()
 
-
 p = {}
 for item in a:
     p[item["id"]] = ((item["nominal"] - item["price"]) + (
-                item["prc"] / 100 * item["nominal"] * (item["year"] - 2021))) / item["price"] * 365 / (
-                                365 * (item["year"] - 2021)) * 100
+            item["prc"] / 100 * item["nominal"] * (item["year"] - 2021))) / item["price"] * 365 / (
+                            365 * (item["year"] - 2021)) * 100
 print(p)
-prc1=0
-prc2=0
+prc1 = 0
+prc2 = 0
+prc3 = 0
+prc4 = 0
 vvod = 50000
-gorizont=2025
-year_now=2022
-while gorizont>=year_now:
-  for item in a:
-    if item["year"]<=gorizont:
-      if item["price"]>(item["nominal"]*0.75): #не обязательно, проверка на дефолт
-        if prc1<p[item["id"]] and year_now<=item["year"]:
-          prc1=p[item["id"]]
-          year1=item["year"]
-          id1=item["id"]
-        if prc2<p[item["id"]] and prc1!=p[item["id"]] and year_now<=item["year"]:
-          prc2=p[item["id"]]
-          year2=item["year"]
-          id2=item["id"]
-  print("Покупаем облигацию ",id1," и ",id2,"с процентами годовых ",prc1," и ",prc2,"до ",year1," и ",year2)
-  if year_now<=year1: print("Покупаем акцию", id1)
-  InvestApi.finish(year_now)
-  year_now=year_now+1
-  prc1=0
-  prc2=0
-
-
+gorizont = 2025
+year_now = 2022
+while gorizont >= year_now:
+    for item in a:
+        if item["year"] <= gorizont:
+            if item["price"] > (item["nominal"] * 0.75):  # не обязательно, проверка на дефолт
+                if prc1 < p[item["id"]] and year_now <= item["year"]:
+                    prc1 = p[item["id"]]
+                    year1 = item["year"]
+                    id1 = item["id"]
+                if prc2 < p[item["id"]] and prc1 != p[item["id"]] and year_now <= item["year"]:
+                    prc2 = p[item["id"]]
+                    year2 = item["year"]
+                    id2 = item["id"]
+                if prc3 < p[item["id"]] and prc1 != p[item["id"]] and prc2 != p[item["id"]] and year_now <= item[
+                    "year"]:
+                    prc3 = p[item["id"]]
+                    year3 = item["year"]
+                    id3 = item["id"]
+                if prc4 < p[item["id"]] and prc1 != p[item["id"]] and prc2 != p[item["id"]] and prc3 != p[
+                    item["id"]] and year_now <= item["year"]:
+                    prc4 = p[item["id"]]
+                    year4 = item["year"]
+                    id4 = item["id"]
+    print("Покупаем облигацию ", id1, id2, id3, id4, "с процентами годовых ", prc1, prc2, prc3, prc4, "до ", year1,
+          year2, year3, year4)
+    InvestApi.finish(year_now)
+    year_now = year_now + 1
+    prc1 = 0
+    prc2 = 0
