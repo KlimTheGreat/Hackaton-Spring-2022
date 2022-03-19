1cca7ad869315d9e5a29ce66be3ab3e1e974fc75
Klim <ya.klim@list.ru>
2022-03-19 04:17:37 +0300

    new library

--- t-invest-api.py
+++ t-invest-api.py
@@ -8,17 +8,29 @@ from tinkoff.investments import (
     Environment,
     TinkoffInvestmentsRESTClient,
 )
+
+from tinkoff.investments.client.exceptions import TinkoffInvestmentsError
 from tinkoff.investments.utils.historical_data import HistoricalData
 
 
-async def get_minute_candles():
-    # show 1 minute candles for AAPL in 1 year period of time
-    async with TinkoffInvestmentsRESTClient(
-        token="t.0d0gyqxYtnN4fZSlh7cdORCpIJ6TNxwk5Ksehg-FOxfRoZGdSeW0TNaLMIeMiZNVQTwUpcWjNGa4AEe7bDzcBQ", environment=Environment.SANDBOX
-    ) as client:
-        bonds = client.market.instruments.get_bonds()
-        ):
-            print(bonds)
+async def get_bonds():
+    try:
+        async with TinkoffInvestmentsRESTClient(
+                token="t.0d0gyqxYtnN4fZSlh7cdORCpIJ6TNxwk5Ksehg-FOxfRoZGdSeW0TNaLMIeMiZNVQTwUpcWjNGa4AEe7bDzcBQ",
+                environment=Environment.SANDBOX
+        ) as client:
+            bonds = await client.market.instruments.get_bonds()
+            historical_data = HistoricalData(client)
+            for bond in bonds:
+                candles = historical_data.iter_candles(
+                        figi=bond.figi,
+                        dt_from=datetime(2022, 1, 12),
+                        dt_to=datetime(2022, 1, 13),
+                        interval=CandleResolution.DAY,
+                )
+                print(bond.isin, ":", candles[0], "руб")
+    except TinkoffInvestmentsError as e:
+        print(e)
 
 
-asyncio.run(get_minute_candles())
+asyncio.get_event_loop().run_until_complete(get_bonds())
